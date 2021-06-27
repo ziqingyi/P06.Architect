004720852ebf87e8e266df2f375d946275067b91
adrian_sun <ziqingyi@qq.com>
2021-06-27 17:54:56 +1000

    Add project files.

---
+++ P06.Architect/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P06.Architect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

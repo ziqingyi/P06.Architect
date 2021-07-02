56764d19132d3f81626ddaadb9835e69390b18ef
adrian_sun <ziqingyi@qq.com>
2021-07-02 23:39:24 +1000

    AllocatorTest add

---
+++ P01.CLRdemo/AllocatorTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.CLRdemo
+{
+    public class AllocatorTest
+    {
+        public static void Show()
+        {
+
+
+
+        }
+
+    }
+
+
+
+    public class TestClass
+    {
+        private TestClass _testClass;
+        public int Id = 123;
+        public string Name = "";
+        public TestClass(int id, string name)
+        {
+            Console.WriteLine($"Default is {this.Id}_{this.Name}");
+            this.Id = id;
+            this.Name = name;
+        }
+        public void Show()
+        {
+            this._testClass = this;
+            TestClass testClass1 = new TestClass(1, "11");
+            for (int i = 0; i < 100; i++)
+            {
+                TestClass m = new TestClass(1, "11"); ;
+            }
+        }
+    }
+
+    public struct TestStruct
+    {
+        public int Id ;//=2 error. cannot have instance property or field initializers in structs.
+        public string Name;
+        public TestStruct(int id, string name)
+        {
+            //Console.WriteLine($"Default is {this.Id}_{this.Name}_{this.GetType().Name}");//error, must use after initialize
+            this.Id = id;
+            this.Name = name;
+            Console.WriteLine($"Now is {this.Id}_{this.Name}_{this.GetType().Name}");
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+}

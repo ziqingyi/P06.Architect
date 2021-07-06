cd3718e21773ced4ec84ca77fa43cfcb929d0657
Adrian Sun <adrian_sun@livingstone.com.au>
2021-07-06 15:51:27 +1000

    MappingExtend for memberInfo, type and property

---
+++ P02.ORMExplore.Framework/SqlMapping/MappingExtend.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace P02.ORMExplore.Framework.SqlMapping
+{
+    public static class MappingExtend
+    {
+
+        //member info can be type of property
+        public static string GetMappingName(this MemberInfo memberInfo)
+        {
+            if (memberInfo.IsDefined(typeof(ORMAbstractMappingAttribute), true))
+            {
+                var attribute = memberInfo.GetCustomAttribute<ORMAbstractMappingAttribute>();
+
+                return attribute.GetName();
+            }
+            else
+            {
+                return memberInfo.Name;
+            }
+        }
+
+        public static string GetTableName(this Type type)
+        {
+            if (type.IsDefined(typeof(ORMdbTableAttribute), true))
+            {
+                ORMdbTableAttribute attribute = type.GetCustomAttribute<ORMdbTableAttribute>();
+                return attribute.GetName();
+            }
+            else
+            {
+                return type.Name;
+            }
+        }
+
+        public static string GetColumnName(this PropertyInfo prop)
+        {
+            if (prop.IsDefined(typeof(ORMdbColumnAttribute), true))
+            {
+                ORMdbColumnAttribute attribute = prop.GetCustomAttribute<ORMdbColumnAttribute>();
+                return attribute.GetName();
+            }
+            else
+            {
+                return prop.Name;
+            }
+
+        }
+
+
+
+
+
+
+    }
+}

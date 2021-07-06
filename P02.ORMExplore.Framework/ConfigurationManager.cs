5da0569407953def94a10d30229358e1eb776fa3
Adrian Sun <adrian_sun@livingstone.com.au>
2021-07-06 16:00:35 +1000

    ConfigurationManager

---
+++ P02.ORMExplore.Framework/ConfigurationManager.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace P02.ORMExplore.Framework
+{
+    public class ConfigurationManager
+    {
+        private static string _sqlConnectionString;
+
+        public static string SqlConnectionString
+        {
+            get
+            {
+                return _sqlConnectionString;
+            }
+        }
+
+
+        static ConfigurationManager()
+        {
+            string directory = Directory.GetCurrentDirectory();
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(directory)
+                .AddJsonFile("appsettings.json");
+
+            IConfigurationRoot configurationRoot = builder.Build();
+            _sqlConnectionString = configurationRoot["connectionString"];
+        }
+
+
+
+
+
+
+    }
+}

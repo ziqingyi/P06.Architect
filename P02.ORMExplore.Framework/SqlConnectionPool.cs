913836eaab2210814c8d4af613a486bfe47a0dd8
adrian_sun <ziqingyi@qq.com>
2021-07-09 21:36:16 +1000

    basic sql conneciton pool

---
+++ P02.ORMExplore.Framework/SqlConnectionPool.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P02.ORMExplore.Framework
+{
+    public enum SqlConnectionType
+    {
+        read,
+        Write
+    }
+    public class SqlConnectionPool
+    {
+
+        public static string GetConnectionString(SqlConnectionType sqlConnectionType)
+        {
+            string conn;
+            switch (sqlConnectionType)
+            {
+                case SqlConnectionType.read:
+                    conn = ConfigurationManager.SqlConnectionString;
+                    break;
+                case SqlConnectionType.Write:
+                    conn = ConfigurationManager.SqlConnectionString;
+                    break;
+                default:
+                    throw new Exception("wrong sql connection type");
+            }
+            return conn;
+        }
+
+
+
+
+
+
+
+    }
+}

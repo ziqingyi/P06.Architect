1ad173990bb49ea861dbc4c764f790ca893ff5ab
Adrian Sun <adrian_sun@livingstone.com.au>
2021-07-13 08:43:36 +1000

    Email Attribute

---
+++ P02.ORMExplore.Framework/Validation/EmailAttribute.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace P02.ORMExplore.Framework.Validation
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class EmailAttribute : AbstractValidateAttribute
+    {
+        private string _EmailRegular = @"^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$";
+
+        public override ValidateErrorModel Validate(object oValue)
+        {
+            bool test1 = oValue != null;
+            bool test2 = !string.IsNullOrWhiteSpace(oValue.ToString());
+            bool test3 = Regex.IsMatch(oValue.ToString(), this._EmailRegular);
+
+            if (!test1)
+            {
+                return new ValidateErrorModel()
+                {
+                    Result = false,
+                    Message = $"{nameof(EmailAttribute)} oValue is null"
+                };
+            }
+            if (!test2)
+            {
+                return new ValidateErrorModel()
+                {
+                    Result = false,
+                    Message = $"{nameof(EmailAttribute)} oValue is empty"
+                };
+            }
+
+            if (!test3)
+            {
+                return new ValidateErrorModel()
+                {
+                    Result = false,
+                    Message = $"{nameof(EmailAttribute)} oValue is null"
+                };
+            }
+            else
+            {
+                return new ValidateErrorModel()
+                {
+                    Result = true,
+                    Message = $"{nameof(EmailAttribute)} all good"
+                };
+            }
+
+        }
+
+    }
+}

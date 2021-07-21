6062cc5b8926795a4520379929afb22ac21ec05a
Adrian_Sun <ziqingyi@qq.com>
2021-07-21 17:43:39 +1000

    config session and data transfer, viewbag, data, temp data, model

--- P03.DotNetCoreMVC/Controllers/AFirstController.cs
+++ P03.DotNetCoreMVC/Controllers/AFirstController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace P03.DotNetCoreMVC.Controllers
@@ -10,7 +11,27 @@ namespace P03.DotNetCoreMVC.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            base.ViewBag.User1 = "userInBag";
+            base.ViewData["User2"] = "userInView";
+            base.TempData["User3"] = "userInTemp";
+            object modelName = "User4";
+
+
+            #region Test Session
+
+            string result = base.HttpContext.Session.GetString("User5");
+
+            if (string.IsNullOrWhiteSpace(result))
+            {
+                base.HttpContext.Session.SetString("User5", "UserInSession");
+            }
+
+
+            #endregion
+
+
+
+            return View(modelName);
         }
     }
 }
--- P03.DotNetCoreMVC/Startup.cs
+++ P03.DotNetCoreMVC/Startup.cs
@@ -23,6 +23,7 @@ namespace P03.DotNetCoreMVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSession();
             services.AddControllersWithViews();
         }
 
@@ -39,6 +40,8 @@ namespace P03.DotNetCoreMVC
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSession();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 

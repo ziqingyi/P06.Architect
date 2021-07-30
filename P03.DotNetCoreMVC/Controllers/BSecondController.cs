efd129a611eae6eca3a7b3b984c108afe2693df1
Adrian_Sun <ziqingyi@qq.com>
2021-07-30 13:31:41 +1000

    BSecondController

---
+++ P03.DotNetCoreMVC/Controllers/BSecondController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace P03.DotNetCoreMVC.Controllers
+{
+    public class BSecondController : Controller
+    {
+        private readonly ILogger<BSecondController> _logger;
+        private readonly ILoggerFactory _loggerFactory;
+
+        public BSecondController(ILogger<BSecondController> logger,
+            ILoggerFactory loggerFactory)
+        {
+            this._logger = logger;
+            this._loggerFactory = loggerFactory;
+        }
+
+
+        public IActionResult Index()
+        {
+            this._logger.LogWarning("this is BSecondController Index");
+
+            return View();
+        }
+    }
+}

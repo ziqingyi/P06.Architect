c153a89caae797e6212f6d6dee7b7e025f35ebb4
Adrian_Sun <ziqingyi@qq.com>
2021-08-02 09:16:37 +1000

    CustomExceptionFilterAttribute check ajax

--- P03.DotNetCoreMVC.Utility/Filters/CustomExceptionFilterAttribute.cs
+++ P03.DotNetCoreMVC.Utility/Filters/CustomExceptionFilterAttribute.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
@@ -8,22 +9,49 @@ namespace P03.DotNetCoreMVC.Utility.Filters
 {
     public class CustomExceptionFilterAttribute : ExceptionFilterAttribute
     {
-
+        
 
 
         public override void OnException(ExceptionContext context)
         {
             if (!context.ExceptionHandled)
             {
-                context.Result = new JsonResult(new
+                //check request: whether it is Ajax request. 
+                //if (context.HttpContext.Request.IsAjax())//.net framework: check header: XMLHttpRequest
+                if(this.IsAjaxReqeust(context.HttpContext.Request))
                 {
-                    Result = false,
-                    Msg = context.Exception.Message
-                });//terminate all following actions.
+                    context.Result = new JsonResult(new
+                    {
+                        Result = false,
+                        Msg = context.Exception.Message
+                    });//terminate all following actions.
 
-                context.ExceptionHandled = true;
+                    context.ExceptionHandled = true;
+                }
+                else
+                {
+                    context.Result = new RedirectResult("/Home/Error");
+                }
+
+
+
+
+
+
             }
             //base.OnException(context);
+        }
+
+
+
+
+
+
+
+        private bool IsAjaxReqeust(HttpRequest request)
+        {
+            string header = request.Headers["X-Requested-With"];
+            return "X-Requested-With".Equals(header);
         }
 
     }

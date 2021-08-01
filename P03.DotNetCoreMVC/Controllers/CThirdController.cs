914d66bb534ce3a64408c8a2a9a933f04b049a09
Adrian_Sun <ziqingyi@qq.com>
2021-08-02 08:31:21 +1000

    third controller

---
+++ P03.DotNetCoreMVC/Controllers/CThirdController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using P03.DotNetCoreMVC.Interface.TestServiceInterface;
+
+namespace P03.DotNetCoreMVC.Controllers
+{
+    public class CThirdController : Controller
+    {
+        #region Identity
+        private readonly ILogger<CThirdController> _logger;
+        private readonly ILoggerFactory _loggerFactory;
+        private readonly ITestServiceA _testServiceA;
+        private readonly ITestServiceB _testServiceB;
+        private readonly ITestServiceC _testServiceC;
+        private readonly ITestServiceD _testServiceD;
+        private readonly ITestServiceE _testServiceE;
+        private readonly IServiceProvider _serviceProvider;
+
+        private readonly IConfiguration _configuration;
+        #endregion
+
+
+        public CThirdController(ILogger<CThirdController> logger,
+            ILoggerFactory loggerFactory
+            , ITestServiceA testServiceA
+            , ITestServiceB testServiceB
+            , ITestServiceC testServiceC
+            , ITestServiceD testServiceD
+            , ITestServiceE testServiceE
+            , IServiceProvider serviceProvider
+            , IConfiguration configuration)
+        {
+            _testServiceA = testServiceA;
+            _testServiceB = testServiceB;
+            _testServiceC = testServiceC;
+            _testServiceD = testServiceD;
+            _testServiceE = testServiceE;
+            _serviceProvider = serviceProvider;
+            _configuration = configuration;
+
+            this._logger = logger;
+            this._loggerFactory = loggerFactory;
+        }
+
+
+        public IActionResult Index()
+        {
+
+
+
+
+
+            string AllowedHost = this._configuration["AllowedHosts"];
+            this._logger.LogWarning("this is CThirdController Index");
+            return View();
+        }
+    }
+}

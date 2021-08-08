078d32d1949bcd3cd52fc9c6d11a18e02fc87cb2
Adrian_Sun <ziqingyi@qq.com>
2021-08-09 08:56:58 +1000

    DFourthController

---
+++ P03.DotNetCoreMVC/Controllers/DFourthController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P03.DotNetCoreMVC.Controllers
+{
+    public class DFourthController : Controller
+    {
+
+
+
+
+
+
+
+
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

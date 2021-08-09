f79a7d7f8b6401bb90e1ccec73fcfca6b16d2100
adrian_sun <ziqingyi@qq.com>
2021-08-09 18:35:55 +1000

    current user model and error view model

---
+++ P03.DotNetCoreMVC.Utility/Models/CurrentUserCore.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace P03.DotNetCoreMVC.Utility.Models
+{
+    public class CurrentUserCore
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Account { get; set; }
+        public string Password { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+        public DateTime LoginTime { get; set; }
+
+        public List<Data> Datas { get; set; }
+    }
+    public class Data
+    { }
+}
---
+++ P03.DotNetCoreMVC.Utility/Models/ErrorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03.DotNetCoreMVC.Utility.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+    }
+}

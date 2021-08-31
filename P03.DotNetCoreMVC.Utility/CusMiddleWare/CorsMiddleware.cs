17400adec5d538f1dc5ff00221125c8e00664b8d
Adrian_Sun <ziqingyi@qq.com>
2021-08-31 16:29:17 +1000

    webapi 3 cors enable

---
+++ P03.DotNetCoreMVC.Utility/CusMiddleWare/CorsMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace P03.DotNetCoreMVC.Utility.CusMiddleWare
+{
+    public class CorsMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CorsMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (!context.Response.Headers.ContainsKey("Access-Control-Allow-Origin"))
+            {
+                context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            }
+
+            await _next(context);
+        }
+
+
+    }
+}

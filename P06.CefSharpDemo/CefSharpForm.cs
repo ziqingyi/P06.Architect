8a38b4beac0687b57286a8e5d98d705f09e12270
Adrian_Sun <ziqingyi@qq.com>
2021-09-09 16:22:06 +1000

    add CefSharp and load in form

---
+++ P06.CefSharpDemo/CefSharpForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace P06.CefSharpDemo
+{
+    public partial class CefSharpForm : Form
+    {
+
+        public ChromiumWebBrowser ChromeBrowser;
+        //private CefCustomObject 
+
+
+        public CefSharpForm()
+        {
+            InitializeComponent();
+
+
+            CefSettings settings = new CefSettings(); 
+            string page = "https://google.com";
+
+            Cef.Initialize(settings);
+
+            ChromeBrowser = new ChromiumWebBrowser(page);
+
+            // Add it to the form and fill it to the form window
+            this.Controls.Add(ChromeBrowser);
+            ChromeBrowser.Dock = DockStyle.Fill;
+
+            //Allow the use of local resources in the browser
+            BrowserSettings browserSettings = new BrowserSettings();
+            browserSettings.FileAccessFromFileUrls = CefState.Enabled;
+            browserSettings.UniversalAccessFromFileUrls = CefState.Enabled;
+            ChromeBrowser.BrowserSettings = browserSettings;
+
+
+        }
+
+
+    private void CefSharpForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

188063d4c3e24fca758cbe9dc4a08195e5083678
Adrian_Sun <ziqingyi@qq.com>
2021-09-09 10:12:29 +1000

    AuthenticationCenter api

---
+++ P03.DotNetCoreMVC.AuthenticationCenter/Controllers/AuthenticationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P03.DotNetCoreMVC.AuthenticationCenter.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+ 
+
+
+
+
+
+
+
+
+
+
+
+    }
+}

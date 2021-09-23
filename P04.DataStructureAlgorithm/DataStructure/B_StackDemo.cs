60316103baf990be3c37bab9d2ff40d41e72dbfb
adrian_sun <ziqingyi@qq.com>
2021-09-23 23:08:55 +1000

    B_StackDemo

---
+++ P04.DataStructureAlgorithm/DataStructure/B_StackDemo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P04.DataStructureAlgorithm.DataStructure
+{
+    public class B_StackDemo
+    {
+        public static void Show()
+        {
+            Console.WriteLine("***************Stack<T>******************");
+            Stack<string> numbers = new Stack<string>();
+            numbers.Push("one");
+            numbers.Push("two");
+            numbers.Push("three");
+            numbers.Push("four");
+            numbers.Push("five");
+
+            foreach (string number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine($"Pop '{numbers.Pop()}'");
+            Console.WriteLine($"Peek at next item to dequeue: { numbers.Peek()}");
+            Console.WriteLine($"Pop '{numbers.Pop()}'");
+
+            Stack<string> stackCopy = new Stack<string>(numbers.ToArray());
+            foreach (string number in stackCopy)
+            {
+                Console.WriteLine(number);
+            }
+
+            //stackCopy.Capacity;
+            stackCopy.TrimExcess();
+
+            Console.WriteLine($"stackCopy.Contains(\"four\") = {stackCopy.Contains("four")}");
+            stackCopy.Clear();
+            Console.WriteLine($"stackCopy.Count = {stackCopy.Count}");
+
+
+        }
+    }
+}

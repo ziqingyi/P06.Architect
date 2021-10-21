bb4f0537203e13a218b2d6f2d985fc6aa94388f5
Adrian_Sun <ziqingyi@qq.com>
2021-10-21 17:43:26 +1100

    DijkstraGraph

---
+++ P04.DataStructureAlgorithm/DataStructure/K_DijkstraDemo.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P04.DataStructureAlgorithm.DataStructure
+{
+    public class K_DijkstraDemo
+    {
+
+
+
+
+
+
+    }
+
+
+
+    public class DistOriginal
+    {
+        public int distance;
+        public int parentVert;
+        public DistOriginal(int pv, int d)
+        {
+            distance = d;
+            parentVert = pv;
+        }
+    }
+    public class DijkstraVertex
+    {
+        public string label;
+        public bool isInTree;
+        public DijkstraVertex(string lab)
+        {
+            label = lab;
+            isInTree = false;
+        }
+    }
+
+    public class DijkstraGraph
+    {
+        private const int max_verts = 20;
+        int infinity = 1000000;
+        DijkstraVertex[] vertexList;
+        int[,] adjMat;
+        int nVerts;
+        int nTree;
+        DistOriginal[] sPath;
+        int currentVert;
+        int startToCurrent;
+
+        public DijkstraGraph()
+        {
+            vertexList = new DijkstraVertex[max_verts];
+            adjMat = new int[max_verts, max_verts];
+            nVerts = 0;
+            nTree = 0;
+            for (int j = 0; j <= max_verts - 1; j++)
+            for (int k = 0; k <= max_verts - 1; k++)
+                adjMat[j, k] = infinity;
+            sPath = new DistOriginal[max_verts];
+        }
+
+        public void AddVertex(string lab)
+        {
+            vertexList[nVerts] = new DijkstraVertex(lab);
+            nVerts++;
+        }
+
+        public void AddEdge(int start, int theEnd, int weight)
+        {
+            adjMat[start, theEnd] = weight;
+        }
+
+        public void Path()
+        {
+            int startTree = 0;
+            vertexList[startTree].isInTree = true;
+            nTree = 1;
+            for (int j = 0; j <= nVerts; j++)
+            {
+                int tempDist = adjMat[startTree, j];
+                sPath[j] = new DistOriginal(startTree, tempDist);
+            }
+
+            while (nTree < nVerts)
+            {
+                int indexMin = GetMin();
+                int minDist = sPath[indexMin].distance;
+                currentVert = indexMin;
+                startToCurrent = sPath[indexMin].distance;
+                vertexList[currentVert].isInTree = true;
+                nTree++;
+                AdjustShortPath();
+            }
+
+            DisplayPaths();
+            nTree = 0;
+            for (int j = 0; j <= nVerts - 1; j++)
+                vertexList[j].isInTree = false;
+        }
+
+        public int GetMin()
+        {
+            int minDist = infinity;
+            int indexMin = 0;
+            for (int j = 1; j <= nVerts - 1; j++)
+                if (!(vertexList[j].isInTree) && sPath[j].distance < minDist)
+                {
+                    minDist = sPath[j].distance;
+                    indexMin = j;
+                }
+
+            return indexMin;
+        }
+
+        public void AdjustShortPath()
+        {
+            int column = 1;
+            while (column < nVerts)
+                if (vertexList[column].isInTree)
+                    column++;
+                else
+                {
+                    int currentToFring = adjMat[currentVert, column];
+                    int startToFringe = startToCurrent + currentToFring;
+                    int sPathDist = sPath[column].distance;
+                    if (startToFringe < sPathDist)
+                    {
+                        sPath[column].parentVert = currentVert;
+                        sPath[column].distance = startToFringe;
+                    }
+
+                    column++;
+                }
+        }
+
+        public void DisplayPaths()
+        {
+            for (int j = 0; j <= nVerts - 1; j++)
+            {
+                Console.Write(vertexList[j].label + "=");
+                if (sPath[j].distance == infinity)
+                    Console.Write("inf");
+                else
+                    Console.Write(sPath[j].distance);
+                string parent = vertexList[sPath[j].parentVert].label;
+                Console.Write("(" + parent + ") ");
+            }
+        }
+
+    }
+}

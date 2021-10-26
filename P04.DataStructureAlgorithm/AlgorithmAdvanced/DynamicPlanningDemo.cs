f64d15d7c261bf6407df7d4360db58836a9fba4b
adrian_sun <ziqingyi@qq.com>
2021-10-26 23:09:23 +1100

    FindLongestCommonSubStringShow

--- P04.DataStructureAlgorithm/AlgorithmAdvanced/DynamicPlanningDemo.cs
+++ P04.DataStructureAlgorithm/AlgorithmAdvanced/DynamicPlanningDemo.cs
@@ -10,16 +10,9 @@ namespace P04.DataStructureAlgorithm.AlgorithmAdvanced
         public static void Show()
         {
             ShowFibonacci();
-
+            FindLongestCommonSubStringShow();
 
         }
-
-
-
-
-
-
-
 
 
 
@@ -116,6 +109,112 @@ namespace P04.DataStructureAlgorithm.AlgorithmAdvanced
 
 
 
+        #region longest common sub string
+
+        public static void FindLongestCommonSubStringShow()
+        {
+            string wordLeft = "eleven";
+            string wordRight = "seven";// "secen";// "sevem";
+            string result = FindLongCommonSubString(wordLeft, wordRight);
+
+            if (string.IsNullOrWhiteSpace(result))
+            {
+                Console.WriteLine("no common string");
+            }
+            else
+            {
+                Console.WriteLine("Longest Common Sub String: " + result);
+            }
+        }
+
+        public static string FindLongCommonSubString(string wordLeft, string wordRight)
+        {
+            string[] warrayLeft = new string[wordLeft.Length];
+            string[] warrayRight = new string[wordRight.Length];
+            string subString;
+            int[,] larray = new int[wordLeft.Length, wordRight.Length];
+            CompareString(wordLeft, wordRight, warrayLeft, warrayRight, larray);
+            Console.WriteLine();
+            ShowArray(larray);
+            subString = GetString(larray, warrayLeft);
+            Console.WriteLine();
+            Console.WriteLine("The strings are: " + wordLeft + " " + wordRight);
+
+            return subString;
+        }
+
+        private static void CompareString(string wordLeft, string wordRight, string[] wordArrayLeft, string[] wordArrayRight, int[,] arr)
+        {
+            int lenLeft = wordLeft.Length;
+            int lenRight = wordRight.Length;
+            for (int k = 0; k <= wordLeft.Length - 1; k++)
+            {
+                wordArrayLeft[k] = wordLeft.Substring(k, 1);
+            }
+            for (int k = 0; k <= wordRight.Length - 1; k++)
+            {
+                wordArrayRight[k] = wordRight.Substring(k, 1);
+            }
+            for (int i = lenLeft - 1; i >= 0; i--)
+            {
+                for (int j = lenRight - 1; j >= 0; j--)
+                {
+                    if (wordArrayLeft[i] == wordArrayRight[j])
+                    {
+                        if (i == lenLeft - 1 || j == lenRight - 1)//if it is the edge point
+                        {
+                            arr[i, j] = 1;
+                        }
+                        else//accumulate for finding the largest num
+                        {
+                            arr[i, j] = 1 + arr[i + 1, j + 1];
+                        }
+                        //arr[i, j] = 1;
+                    }
+                    else
+                    {
+                        arr[i, j] = 0;
+                    }
+                }
+            }
+        }
+        ///find the largest common string in this array
+        private static string GetString(int[,] arr, string[] wordArrayLeft)
+        {
+            string subString = "";
+            int max = 0;
+            int leftIndex = 0;
+            for (int i = 0; i <= arr.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= arr.GetUpperBound(1); j++)
+                {
+                    if (arr[i, j] > max)
+                    {
+                        max = arr[i, j];
+                        leftIndex = i;
+                    }
+                }
+            }
+            for (int i = 0; i < max; i++)
+            {
+                subString += wordArrayLeft[leftIndex + i];
+            }
+            return subString;
+        }
+        //display the array
+        private static void ShowArray(int[,] arr)
+        {
+            Console.WriteLine("Display the Array");
+            for (int row = 0; row <= arr.GetUpperBound(0); row++)
+            {
+                for (int col = 0; col <= arr.GetUpperBound(1); col++)
+                {
+                    Console.Write(arr[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        #endregion
 
 
 

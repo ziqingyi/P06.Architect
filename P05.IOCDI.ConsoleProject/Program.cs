08699c9b36ff59b34f14b4962ada139ef0b761c8
Adrian_Sun <ziqingyi@qq.com>
2021-11-17 09:35:54 +1100

    console test proj

---
+++ P05.IOCDI.ConsoleProject/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class Projgram
+{
+
+    public static void Main(string[] args)
+    {
+        #region tightly coupled
+        //{
+        //    //high level rely(dependent) on lower level,
+        //    //lower level update will lead to changes in higher level.
+
+        //    UserDAL userDAL = new UserDAL();
+        //    UserBLL userBLL = new UserBLL(userDAL);
+        //    userBLL.Login("Administrator");
+        //}
+
+        #endregion
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+

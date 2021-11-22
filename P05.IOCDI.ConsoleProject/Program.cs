49c419f52f105340af228772e5a569a44f1e98c7
Adrian_Sun <ziqingyi@qq.com>
2021-11-22 17:45:02 +1100

    test resolve with reflection

--- P05.IOCDI.ConsoleProject/Program.cs
+++ P05.IOCDI.ConsoleProject/Program.cs
@@ -45,13 +45,14 @@ public class Projgram
 
         #region  factory 3: IOC (register and resolve)
 
-        //1 create by factory, but user need to know the params for each obj, relationship --> 
+        //1 create by factory, but user need to know the params for each obj, relationship --> container find ctors and params
         {
             IContainer container = new CustomContainer();
             container.Register<ITestServiceA, TestServiceA>();
-            ITestServiceA service = container.Resolve<ITestServiceA>();
+            ITestServiceA serviceA = container.Resolve<ITestServiceA>();
 
-
+            container.Register<ITestServiceB, TestServiceB>();
+            ITestServiceB serviceB = container.Resolve<ITestServiceB>();
         }
 
         #endregion
--- P05.IOCDI.Framework/CustomContainerFolder/CustomContainer.cs
+++ P05.IOCDI.Framework/CustomContainerFolder/CustomContainer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,7 +20,7 @@ namespace P05.IOCDI.Framework.CustomContainerFolder
         public TService Resolve<TService>()
         {
             string key = typeof(TService).FullName!;
-            if(containerDic.ContainsKey(key))
+            if(!containerDic.ContainsKey(key))
             {
                 throw new Exception($"{key} not registered");
             }
@@ -32,10 +33,40 @@ namespace P05.IOCDI.Framework.CustomContainerFolder
         private object Resolve(Type type)
         {
 
+            #region 1 IOC 
+            //find all ctors
+            ConstructorInfo[] constractorArray = type.GetConstructors();
+
+            #region find a ctor from the list
+
+            ConstructorInfo ctor0 = constractorArray[0];
+
+
+
+            #endregion
+
+            //get all parameters of this ctor
+            var parameterArray = ctor0.GetParameters();
+
+            //
+            List<object> oParameterInstanceList = new List<object>();
+            foreach (var p in parameterArray)
+            {
+                Type parameterType = p.ParameterType;
+                Type targetType = this.containerDic[parameterType.FullName!];
+                object parameterInsance = Activator.CreateInstance(targetType)!;
+                oParameterInstanceList.Add(parameterInsance);
+            }
+
+            #endregion
+
+
+
+
 
 
             #region create instance with reflection
-            object oInstance = Activator.CreateInstance(type);
+            object oInstance = Activator.CreateInstance(type,oParameterInstanceList.ToArray())!;
             return oInstance;
             #endregion
         }

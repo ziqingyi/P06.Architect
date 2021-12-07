c9f5a16cdc75a72e6dd85691c046fcc67a8d14fe
Adrian_Sun <ziqingyi@qq.com>
2021-12-07 17:49:35 +1100

    email option

---
+++ P03.DotNetCoreMVC/Models/EmailOption.cs
@@ -0,0 +1,11 @@
+﻿namespace P03.DotNetCoreMVC.Models
+{
+    public class EmailOption
+    {
+        public string Title { get; set; }
+
+        public string Body { get; set; }
+
+        public string From { get; set; }
+    }
+}

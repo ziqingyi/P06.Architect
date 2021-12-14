015135ea83e85c059c09f0026c96991100712554
Adrian_Sun <ziqingyi@qq.com>
2021-12-15 09:04:29 +1100

    global exception

--- P03.DotNetCoreMVC/Startup.cs
+++ P03.DotNetCoreMVC/Startup.cs
@@ -436,6 +436,7 @@ namespace P03.DotNetCoreMVC
             }
             else
             {
+                //handle global excepition
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();

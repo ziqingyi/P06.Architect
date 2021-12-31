83fe08fa69d3d4ff1d970d2f5236258c07834041
adrian_sun <ziqingyi@qq.com>
2021-12-31 12:09:49 +1100

    new test api 3

---
+++ P03.DotNetCoreMVC.WebApi2/Controllers/aFirstTestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P03.DotNetCoreMVC.WebApi2.Controllers
+{
+    public class aFirstTestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

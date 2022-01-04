a1cf70b25e5acadeaa995aa8d55282a1b67d5bce
Adrian_Sun <ziqingyi@qq.com>
2022-01-04 17:35:10 +1100

    add unit test for api

---
+++ P03.DotNetCoreMVC.WebNUnitTestProject/UnitTest1.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework;
+using System;
+using System.Net.Http;
+
+namespace P03.DotNetCoreMVC.WebNUnitTestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            string url = "http://localhost:44357/api/aFirstTest/getstring";
+
+            using(HttpClient httpClient = new HttpClient())
+            {
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.Method = HttpMethod.Get;
+                message.RequestUri = new Uri(url);
+                var result = httpClient.SendAsync(message).Result;
+
+                string content = result.Content.ReadAsStringAsync().Result;
+
+            }
+
+
+
+
+
+
+
+
+
+            //Assert.Pass();
+        }
+    }
+}

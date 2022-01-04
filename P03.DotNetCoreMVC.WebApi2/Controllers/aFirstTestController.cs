d07dd43138785e1a6fcff8080276587885e6dd21
Adrian_Sun <ziqingyi@qq.com>
2022-01-04 11:21:57 +1100

    log4 for first test

--- P03.DotNetCoreMVC.WebApi2/Controllers/aFirstTestController.cs
+++ P03.DotNetCoreMVC.WebApi2/Controllers/aFirstTestController.cs
@@ -1,12 +1,61 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace P03.DotNetCoreMVC.WebApi2.Controllers
 {
-    public class aFirstTestController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class aFirstTestController : ControllerBase
     {
-        public IActionResult Index()
+
+        private readonly ILogger<aFirstTestController> _logger;
+
+        public aFirstTestController(ILogger<aFirstTestController> logger)
         {
-            return View();
+            _logger = logger;
+            _logger.LogInformation($"{nameof(aFirstTestController)} is initialized....");
         }
+
+        [HttpGet]
+        [Route("GetString")]
+        public string GetString()
+        {
+            _logger.LogInformation("GetString");
+
+            return "";
+        }
+
+
+        [HttpGet]
+        [Route("GetInt")]
+        public int GetInt(int i)
+        {
+            _logger.LogInformation($"GetInt, param is ：{i}");
+            return i;
+        }
+
+
+        [HttpGet]
+        [Route("GetJson")]
+        public string GetJson(int id, string name)
+        {
+            _logger.LogInformation($"GetJson, params are ：{id}, {name}");
+            return Newtonsoft.Json.JsonConvert.SerializeObject(new
+            {
+                Id = id,
+                Name = name
+            });
+        }
+
+
+
+
+
+
+
+
+
+
     }
 }

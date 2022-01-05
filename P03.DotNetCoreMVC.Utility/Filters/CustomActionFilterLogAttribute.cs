edcd73d886f7f97ea2afdafa8b41de2136d4cb54
Adrian_Sun <ziqingyi@qq.com>
2022-01-05 15:59:36 +1100

    CustomActionFilterLogAttribute with TypeFilter and ServiceFilter

---
+++ P03.DotNetCoreMVC.Utility/Filters/CustomActionFilterLogAttribute.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03.DotNetCoreMVC.Utility.Filters
+{
+    public class CustomActionFilterLogAttribute:ActionFilterAttribute
+    {
+        private ILogger<CustomActionFilterLogAttribute> _logger;
+        public CustomActionFilterLogAttribute(ILogger<CustomActionFilterLogAttribute> logger)
+        {
+            _logger = logger;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            _logger.LogInformation($" this is {nameof(CustomActionFilterLogAttribute)} OnActionExecuting" +
+                              $" with order {this.Order}");
+
+            var actionLog = $"{DateTime.Now} is calling {context.RouteData.Values["action"]} api; Parameter:{Newtonsoft.Json.JsonConvert.SerializeObject(context.ActionArguments)}";
+            _logger.LogInformation(actionLog);
+
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            _logger.LogInformation($" this is {nameof(CustomActionFilterLogAttribute)} OnActionExecuted" +
+                              $" with order {this.Order}");
+
+            var result = context.Result;
+            ObjectResult objectResult = result as ObjectResult;
+            var actionLog = $"{DateTime.Now} executed {context.RouteData.Values["action"]} api; Result:{Newtonsoft.Json.JsonConvert.SerializeObject(objectResult.Value)}";
+            _logger.LogInformation(actionLog);
+        }
+
+
+
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            _logger.LogInformation($" this is {nameof(CustomActionFilterLogAttribute)} OnResultExecuting" +
+                              $" with order {this.Order}");
+            //base.OnResultExecuting(context);
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext context)
+        {
+            _logger.LogInformation($" this is {nameof(CustomActionFilterLogAttribute)} OnResultExecuted" +
+                              $" with order {this.Order}");
+            //base.OnResultExecuted(context);
+        }
+
+
+
+    }
+}

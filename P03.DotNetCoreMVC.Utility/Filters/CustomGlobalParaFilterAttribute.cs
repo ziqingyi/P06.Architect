92ca0e569e3176aa7949db98e149ed896dad8525
Adrian_Sun <ziqingyi@qq.com>
2022-01-07 15:40:41 +1100

    CustomGlobalParaFilterAttribute, global attribute with para

---
+++ P03.DotNetCoreMVC.Utility/Filters/CustomGlobalParaFilterAttribute.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03.DotNetCoreMVC.Utility.Filters
+{
+    public class CustomGlobalParaFilterAttribute : ActionFilterAttribute
+    {
+        private ILogger<CustomGlobalParaFilterAttribute> _logger = null;
+
+        public CustomGlobalParaFilterAttribute(ILogger<CustomGlobalParaFilterAttribute> logger)
+        {
+            this._logger = logger;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            _logger.LogInformation($" this is {nameof(CustomGlobalParaFilterAttribute)} OnActionExecuting");
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            _logger.LogInformation($" this is {nameof(CustomGlobalParaFilterAttribute)} OnActionExecuted");
+        }
+
+
+
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            _logger.LogInformation($" this is {nameof(CustomGlobalParaFilterAttribute)} OnResultExecuting");
+            //base.OnResultExecuting(context);
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext context)
+        {
+            _logger.LogInformation($" this is {nameof(CustomGlobalParaFilterAttribute)} OnResultExecuted");
+            //base.OnResultExecuted(context);
+        }
+    }
+}

10c00b367cdb7f4ba2d90e53ec81d9afc09172ea
Adrian_Sun <ziqingyi@qq.com>
2022-01-19 09:43:38 +1100

    P03.DotNetCoreMVC.EntityFrameworkModels.TestProject

---
+++ P03.DotNetCoreMVC.EntityFrameworkModels.TestProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P03.DotNetCoreMVC.EntityFrameworkModels.TestProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

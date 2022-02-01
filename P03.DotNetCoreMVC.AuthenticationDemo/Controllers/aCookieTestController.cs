c160ba5e4a942e9c36754fefaad7097d2c2c4f85
Adrian_Sun <ziqingyi@qq.com>
2022-02-01 17:40:01 +1100

    aCookieTestController

---
+++ P03.DotNetCoreMVC.AuthenticationDemo/Controllers/aCookieTestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P03.DotNetCoreMVC.AuthenticationDemo.Controllers
+{
+    public class aCookieTestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult Login(string name, string password)
+        {
+            //.net core MVC Cookie, use extend method, keep 30 minutes
+            base.HttpContext.SetCookies("CurrentUser",
+                Newtonsoft.Json.JsonConvert.SerializeObject(currentUser));
+        }
+
+
+    }
+}
--- P03.DotNetCoreMVC.AuthenticationDemo/Startup.cs
+++ P03.DotNetCoreMVC.AuthenticationDemo/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,24 @@ namespace P03.DotNetCoreMVC.AuthenticationDemo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+
+            #region add Authentication, 
+            
+            //just add the service, no need to UseAuthentication() if filtered by attribute
+
+            services.AddAuthentication(options => {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            }).AddCookie();
+
+            #endregion
+
+
+
+
+
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

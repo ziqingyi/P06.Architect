40a99bd7b47e8227600e20ae0b82e92f7f74f7ef
adrian_sun <ziqingyi@qq.com>
2022-02-15 12:02:46 +1100

    JWT HS and RS interfaces

---
+++ P03.DotNetCoreMVC.AuthenticationCenter/ProjectUtility/JWTUtilityUpgradeV1/IJWTRSService.cs
@@ -0,0 +1,9 @@
+﻿using P03.DotNetCoreMVC.Utility.Models;
+
+namespace P03.DotNetCoreMVC.AuthenticationCenter.ProjectUtility.JWTUtilityUpgradeV1
+{
+    public interface IJWTRSService
+    {
+        string GetToken(CurrentUserCore currentUserInfo);
+    }
+}

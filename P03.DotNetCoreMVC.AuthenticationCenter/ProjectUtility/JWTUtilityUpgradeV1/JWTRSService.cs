c7e96aa2b46fd6b4e1743385fedd057e094b159a
adrian_sun <ziqingyi@qq.com>
2022-02-16 14:12:01 +1100

    JWTRSService and RSAHelper

---
+++ P03.DotNetCoreMVC.AuthenticationCenter/ProjectUtility/JWTUtilityUpgradeV1/JWTRSService.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using P03.DotNetCoreMVC.Utility.AuthHelper;
+using P03.DotNetCoreMVC.Utility.Models;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.IO;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace P03.DotNetCoreMVC.AuthenticationCenter.ProjectUtility.JWTUtilityUpgradeV1
+{
+    public class JWTRSService : IJWTRSService
+    {
+
+        #region Inject option
+
+        private readonly JWTTokenOptions _jWTTokenOptions;
+        public JWTRSService(IOptionsMonitor<JWTTokenOptions> jwtTokenOptions)
+        {
+            this._jWTTokenOptions = jwtTokenOptions.CurrentValue;
+        }
+
+        #endregion
+
+        public string GetToken(CurrentUserCore currentUserInfo)
+        {
+            var claims = new[]
+           {
+                new Claim("Id",currentUserInfo.Id.ToString()),
+                new Claim("Account",currentUserInfo.Account),
+                new Claim(ClaimTypes.Name, currentUserInfo.Name),
+                new Claim(ClaimTypes.Email, currentUserInfo.Email),
+                new Claim("Role",currentUserInfo.Role)
+            };
+
+            string keyDir = Directory.GetCurrentDirectory();
+                
+            if(RSAHelper.TryGetKeyParameters(keyDir, true,out RSAParameters keyParams)   == false)
+            {
+                keyParams = RSAHelper.GenerateAndSaveKey(keyDir);
+            }
+
+            var credentials = new SigningCredentials(new RsaSecurityKey(keyParams), SecurityAlgorithms.RsaSha256Signature);
+
+            var token = new JwtSecurityToken(
+                issuer: this._jWTTokenOptions.Issuer,
+                audience: this._jWTTokenOptions.Audience,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(5),
+                signingCredentials: credentials
+                );
+
+            var handler = new JwtSecurityTokenHandler();
+            string tokenString = handler.WriteToken(token);
+            return tokenString;
+
+        }
+    }
+}
---
+++ P03.DotNetCoreMVC.Utility/AuthHelper/RSAHelper.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace P03.DotNetCoreMVC.Utility.AuthHelper
+{
+    public class RSAHelper
+    {
+        //read RSA key from local file
+        public static bool TryGetKeyParameters(string filePath, bool withPrivate, out RSAParameters keyParameters)
+        {
+            string filename = withPrivate ? "key.json" : "key.public.json";
+            string fileFullPath = Path.Combine(filePath, filename);
+
+            keyParameters = default(RSAParameters);
+            if(!File.Exists(fileFullPath))
+            {
+                return false;
+            }
+            else
+            {
+                keyParameters = JsonConvert.DeserializeObject<RSAParameters>(File.ReadAllText(fileFullPath));
+                return true;
+            }
+        }
+
+
+        //generate and save private key and public key, keep in   parameter:  filePath
+        public static RSAParameters GenerateAndSaveKey(string filePath, bool withPrivate = true)
+        {
+            RSAParameters publicKeys, privateKeys;
+            using(var rsa = new RSACryptoServiceProvider(2048))
+            {
+                try
+                {
+                    privateKeys = rsa.ExportParameters(true);
+                    publicKeys = rsa.ExportParameters(false);
+                }
+                finally
+                {
+                    //     Gets or sets a value indicating whether the key should be persisted in the cryptographic
+                    //     service provider (CSP).
+                    rsa.PersistKeyInCsp = false;
+                }
+
+                File.WriteAllText(Path.Combine(filePath, "key.json"), JsonConvert.SerializeObject(privateKeys));
+                File.WriteAllText(Path.Combine(filePath, "key.public.json"), JsonConvert.SerializeObject(publicKeys));
+
+                return withPrivate ? privateKeys : publicKeys;
+            }
+
+        }
+
+
+
+
+
+    }
+}

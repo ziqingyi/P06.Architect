e150ccb254fd460328ebfa43532c0c58c6cbef86
adrian_sun <ziqingyi@qq.com>
2022-02-17 14:13:39 +1100

    P03.DotNetCoreMVC.AuthenticationDemo.JWT

---
+++ P03.DotNetCoreMVC.AuthenticationDemo.JWT/Controllers/aJWTController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P03.DotNetCoreMVC.AuthenticationDemo.JWT.Controllers
+{
+    public class aJWTController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return new JsonResult(new
+            //{
+            //    Result = true,
+            //    Message = "Log out Successfully"
+            //});
+
+
+
+            return View();
+        }
+    }
+}

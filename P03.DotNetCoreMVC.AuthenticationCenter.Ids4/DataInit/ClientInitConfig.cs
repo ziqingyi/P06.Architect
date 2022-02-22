171d02245a47c69242982b9fd88a5bd2b907c2df
adrian_sun <ziqingyi@qq.com>
2022-02-23 09:48:24 +1100

    config id4 (client credentials) in startup and ClientInitConfig

---
+++ P03.DotNetCoreMVC.AuthenticationCenter.Ids4/DataInit/ClientInitConfig.cs
@@ -0,0 +1,44 @@
+﻿
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace P03.DotNetCoreMVC.AuthenticationCenter.Ids4.DataInit
+{
+    public class ClientInitConfig
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new[]
+            {
+                new ApiResource("UserApi", "User API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientId = "AuthenticationCenter.Ids4.DataInit",//Client Id
+                    ClientSecrets = new [] { new Secret("test123123".Sha256()) },//client security
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    
+                    AllowedScopes = new [] { "UserApi" },//accessible resources
+                    Claims=new List<ClientClaim>(){
+                        new ClientClaim(IdentityModel.JwtClaimTypes.Role,"Admin"),
+                        new ClientClaim(IdentityModel.JwtClaimTypes.NickName,"Admin"),
+                        new ClientClaim(ClaimTypes.Email,"abcdefg@gmail.com")
+                    }
+                }
+            };
+        }
+
+
+
+    }
+}
--- P03.DotNetCoreMVC.AuthenticationCenter.Ids4/Startup.cs
+++ P03.DotNetCoreMVC.AuthenticationCenter.Ids4/Startup.cs
@@ -4,10 +4,13 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using P03.DotNetCoreMVC.AuthenticationCenter.Ids4.DataInit;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -26,6 +29,22 @@ namespace P03.DotNetCoreMVC.AuthenticationCenter.Ids4
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            //#region replace with views
+            //services.AddControllersWithViews();
+            //#endregion
+
+            #region  client credentials
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryClients(ClientInitConfig.GetClients())
+                .AddInMemoryApiResources(ClientInitConfig.GetApiResources());
+            #endregion
+
+
+
+
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +56,31 @@ namespace P03.DotNetCoreMVC.AuthenticationCenter.Ids4
             }
 
             app.UseHttpsRedirection();
+
+
+
+            #region config 1
+            //wwwroot folder is must for running in console
+            StaticFileOptions sfo = new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(),
+                        @"wwwroot"))
+            };
+            app.UseStaticFiles(sfo);
+            #endregion
+
+            #region IdentityServer Middleware
+
+            app.UseIdentityServer();
+
+            #endregion
+
+
+
+
+
+
 
             app.UseRouting();
 

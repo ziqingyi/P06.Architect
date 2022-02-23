e8927651f120d6313af6cc7de1a760a3a90580e8
adrian_sun <ziqingyi@qq.com>
2022-02-23 16:36:00 +1100

    Ids4Controller

--- P03.DotNetCoreMVC.AuthenticationDemo.Ids4/Controllers/Ids4Controller.cs
+++ P03.DotNetCoreMVC.AuthenticationDemo.Ids4/Controllers/Ids4Controller.cs
@@ -5,7 +5,58 @@ namespace P03.DotNetCoreMVC.AuthenticationDemo.Ids4.Controllers
 {
     public class Ids4Controller : Controller
     {
-       
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+        [Authorize(Roles = "Admin")]
+        public IActionResult IndexRole()
+        {
+            return new JsonResult(new
+            {
+                Result = true,
+                Message = "Ids4Controller IndexRole "
+            });
+
+        }
+        [Authorize(Policy = "AdminPolicy")]
+        public IActionResult IndexPolicy()
+        {
+            return new JsonResult(new
+            {
+                Result = true,
+                Message = "Ids4Controller IndexPolicy "
+            });
+
+        }
+
+        [Authorize(Policy = "MailPolicy")]
+        public IActionResult IndexEmails()
+        {
+            return new JsonResult(new
+            {
+                Result = true,
+                Message = "Ids4Controller IndexEmails "
+            });
+
+        }
+
+        //[AllowAnonymous]
+        //public IActionResult IndexToken()
+        //{
+        //    return View();
+        //}
+
+        //[AllowAnonymous]
+        //public IActionResult IndexCodeToken()
+        //{
+        //    
+        //    return View();
+        //}
+
+
 
     }
 }

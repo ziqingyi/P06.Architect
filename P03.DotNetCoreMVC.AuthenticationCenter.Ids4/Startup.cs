a068d715501ed24c8faf23710622f8fff0d7bab5
adrian_sun <ziqingyi@qq.com>
2022-02-23 13:38:19 +1100

    add quickstart UI, fix port

--- P03.DotNetCoreMVC.AuthenticationCenter.Ids4/Startup.cs
+++ P03.DotNetCoreMVC.AuthenticationCenter.Ids4/Startup.cs
@@ -16,6 +16,13 @@ using System.Threading.Tasks;
 
 namespace P03.DotNetCoreMVC.AuthenticationCenter.Ids4
 {
+    //dotnet P03.DotNetCoreMVC.AuthenticationCenter.Ids4.dll  --urls="https://*:44398" --ip="127.0.0.1"
+    /* quickstart UI
+     https://github.com/IdentityServer/IdentityServer4.Quickstart.UI
+          //dotnet new -i identityserver4.templates
+          //dotnet new is4ui
+
+     */
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -28,10 +35,10 @@ namespace P03.DotNetCoreMVC.AuthenticationCenter.Ids4
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
-            //#region replace with views
-            //services.AddControllersWithViews();
-            //#endregion
+            //services.AddControllers();
+            #region replace with views
+            services.AddControllersWithViews();
+            #endregion
 
             #region  client credentials
             services.AddIdentityServer()

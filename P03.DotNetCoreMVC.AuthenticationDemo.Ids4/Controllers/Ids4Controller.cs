59bdb4bb58650fa0a5bab6326c48a901f6cfc398
adrian_sun <ziqingyi@qq.com>
2022-02-23 16:35:35 +1100

    P03.DotNetCoreMVC.AuthenticationDemo.Ids4

---
+++ P03.DotNetCoreMVC.AuthenticationDemo.Ids4/Controllers/Ids4Controller.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace P03.DotNetCoreMVC.AuthenticationDemo.Ids4.Controllers
+{
+    public class Ids4Controller : Controller
+    {
+       
+
+    }
+}

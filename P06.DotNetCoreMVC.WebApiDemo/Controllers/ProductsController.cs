ec2744b9b1c05a9553526019a2c2f51c5490e159
adrian_sun <ziqingyi@qq.com>
2022-04-18 12:26:13 +1000

    GetProducts by department

--- P06.DotNetCoreMVC.WebApiDemo/Controllers/ProductsController.cs
+++ P06.DotNetCoreMVC.WebApiDemo/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using P06.DotNetCoreMVC.WebApiDemo.Extensions;
@@ -24,15 +25,50 @@ namespace P06.DotNetCoreMVC.WebApiDemo.Controllers
         }
 
 
+        //[HttpGet]
+        //[Route("")]
+        //[ProducesResponseType(StatusCodes.Status200OK)]
+        //public ActionResult<IQueryable<Product>> GetProducts()
+        //{
+        //    var result = _context.Products as IQueryable<Product>;
+
+        //    return Ok(result.OrderBy(p => p.ProductNumber));
+        //}
+
+
         [HttpGet]
         [Route("")]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public ActionResult<IQueryable<Product>> GetProducts()
+        public ActionResult<IQueryable<Product>> GetProducts([FromQuery] string department ,[FromQuery] ProductRequest request)
         {
             var result = _context.Products as IQueryable<Product>;
+            
+            if (!string.IsNullOrEmpty(department))
+            {
+                result = result.Where(p => p.Department.StartsWith(department,
+                    StringComparison.InvariantCultureIgnoreCase));
+            }
+            
+            Response.Headers["x-total-count"] = result.Count().ToString();
 
-            return Ok(result.OrderBy(p => p.ProductNumber));
+            return Ok(result
+                .OrderBy(p => p.ProductNumber)
+                .Skip(request.Offset)
+                .Take(request.Limit));
+
         }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ P06.DotNetCoreMVC.WebApiDemo/Models/ProductRequest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P06.DotNetCoreMVC.WebApiDemo.Models
+{
+    public class ProductRequest
+    {
+        [FromQuery(Name = "limit")]
+        public int Limit { get; set; } = 50;
+
+        [FromQuery(Name = "offset")]
+        public int Offset { get; set; }
+    }
+}

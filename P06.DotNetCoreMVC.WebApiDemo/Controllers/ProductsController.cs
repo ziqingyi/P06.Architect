f5352172c64322402115cbf8f6f3afd7735e82db
adrian_sun <ziqingyi@qq.com>
2022-04-18 11:24:06 +1000

    test product api

---
+++ P06.DotNetCoreMVC.WebApiDemo/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using P06.DotNetCoreMVC.WebApiDemo.Extensions;
+using P06.DotNetCoreMVC.WebApiDemo.Models;
+
+namespace P06.DotNetCoreMVC.WebApiDemo.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("v{version:apiVersion}/[controller]")]
+    [Produces("application/json")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductContext _context;
+
+        public ProductsController(ProductContext context)
+        {
+            _context = context;
+
+            if (_context.Products.Any()) return;
+
+            ProductSeed.InitData(context);
+        }
+
+
+        [HttpGet]
+        [Route("")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IQueryable<Product>> GetProducts()
+        {
+            var result = _context.Products as IQueryable<Product>;
+
+            return Ok(result.OrderBy(p => p.ProductNumber));
+        }
+
+
+
+    }
+}

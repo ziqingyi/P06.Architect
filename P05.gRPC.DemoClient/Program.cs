485f07532de29f4bee4c62ec81a29e9a4fbc8483
adrian_sun <ziqingyi@qq.com>
2022-04-20 08:46:46 +1000

    add grpc service to client

--- P05.gRPC.DemoClient/Program.cs
+++ P05.gRPC.DemoClient/Program.cs
@@ -1,22 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using Grpc.Net.Client;
+using P05.gRPC.DemoServer;
+
+Console.WriteLine("Hello, World!");
+
+TestHello().Wait();
+
+static async Task TestHello()
+{
+        using (var channel = GrpcChannel.ForAddress("https://localhost:5001"))
+        {
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "User1" });
+            Console.WriteLine("Greeter reply : " + reply.Message);
+
+            //client.SayHello(new HelloRequest { Name = "Eleven" });
+        }
 
 
-
-
-
-
-
-
-    Console.WriteLine("Hello, World!");
-
-    static async Task TestHello()
-    {
-
-
-
-    }
+}
 
 
 

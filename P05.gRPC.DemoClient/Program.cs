5e435b10365b1f9c179774a37bbcb075d608b497
adrian_sun <ziqingyi@qq.com>
2022-04-19 14:11:23 +1000

    grpc demo

---
+++ P05.gRPC.DemoClient/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+
+
+
+
+
+
+    Console.WriteLine("Hello, World!");
+
+    static async Task TestHello()
+    {
+
+
+
+    }
+
+
+
+
+

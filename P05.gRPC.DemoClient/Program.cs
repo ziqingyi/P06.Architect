1c0e56b87325e4d56bfba6ada9b94078a0f8a554
adrian_sun <ziqingyi@qq.com>
2022-04-21 14:24:49 +1000

    test CustomMathService

--- P05.gRPC.DemoClient/Program.cs
+++ P05.gRPC.DemoClient/Program.cs
@@ -1,8 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using System.Text.Json.Serialization;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
 using Grpc.Net.Client;
 using P05.gRPC.DemoServer;
+using Newtonsoft.Json;
 
 Console.WriteLine("Hello, World!");
 
@@ -23,14 +27,114 @@ static async Task TestHello()
 
 static async Task TestMath()
 {
+    #region token
+    string token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiRWxldmVuIiwiRU1haWwiOiI1NzI2NTE3N0BxcS5jb20iLCJBY2NvdW50IjoieHV5YW5nQHpoYW94aUVkdS5OZXQiLCJBZ2UiOiIzMyIsIklkIjoiMTIzIiwiTW9iaWxlIjoiMTg2NjQ4NzY2NzEiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJBZG1pbiIsIlNleCI6IjEiLCJuYmYiOjE1OTA1OTQ1OTEsImV4cCI6MTU5MDU5ODEzMSwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo1NzI2IiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdDo1NzI2In0.MGXKLQ9ZVh0xvsQ1kNhb5gXi_8hqD2RL8metxhjEFiU";
+    var headers = new Metadata { { "Authorization", $"Bearer {token}" } };
+    #endregion
+     
     using (var channel = GrpcChannel.ForAddress("https://localhost:5001"))
     {
         var client = new CustomMath.CustomMathClient(channel);
-        var reply = await client.SayHelloAsync(new HelloRequestMath { Name = "User1",Id = 10});
-        Console.WriteLine("Greeter reply : " + reply.Message);
 
-        client.SayHello(new HelloRequestMath { Name = "User2" });
+        Console.WriteLine("***************one time call************");
+        {
+            var reply = await client.SayHelloAsync(new HelloRequestMath { Name = "User1", Id = 10 });
+            Console.WriteLine("Greeter reply : " + reply.Message);
+        }
+
+        Console.WriteLine("***********************call once async********************************");
+        {
+            RequestPara requestPara = new RequestPara() { ILeft = 123, IRight = 234 };
+            var replyPlus = await client.PlusAsync(requestPara);
+            Console.WriteLine($"CustomMath {Thread.CurrentThread.ManagedThreadId}  reply result :{replyPlus.Result}  Massage={replyPlus.Message}");
+        }
+        Console.WriteLine("***********************sync********************************");
+        {
+            var replyPlusSync = client.Plus(new RequestPara() { ILeft = 123, IRight = 234 });
+            Console.WriteLine($"CustomMath {Thread.CurrentThread.ManagedThreadId}   reply result :{replyPlusSync.Result}  Massage={replyPlusSync.Message}");
+        }
+        Console.WriteLine("***********************async with Token********************************");
+        {
+            RequestPara requestPara = new RequestPara() { ILeft = 123, IRight = 234 };
+
+            var replyPlus = await client.PlusAsync(requestPara, headers);
+            Console.WriteLine($"CustomMath {Thread.CurrentThread.ManagedThreadId}   reply result :{replyPlus.Result}  Massage={replyPlus.Message}");
+        }
+
+        Console.WriteLine("***********************sync with Token********************************");
+        {
+            RequestPara requestPara = new RequestPara() { ILeft = 123, IRight = 234 };
+            var replyPlusSync = client.Plus(requestPara, headers);
+            Console.WriteLine($"CustomMath {Thread.CurrentThread.ManagedThreadId}   reply result :{replyPlusSync.Result}  Massage={replyPlusSync.Message}");
+        }
+
+        Console.WriteLine("**************************empty request*****************************");
+        {
+            var countResult = await client.CountAsync(new Empty());
+            Console.WriteLine($"random {countResult.Count}");
+            var rand = new Random(DateTime.Now.Millisecond);
+        }
+        Console.WriteLine("**************************client flow *****************************");
+        {
+            var bathCat = client.SelfIncreaseClient();
+            for (int i = 0; i < 10; i++)
+            {
+                await bathCat.RequestStream.WriteAsync(new BathTheCatReq() { Id = new Random().Next(0, 20) });
+                await Task.Delay(100);
+                Console.WriteLine($"This is {i} Request {Thread.CurrentThread.ManagedThreadId}");
+            }
+            Console.WriteLine("**********************************");
+            
+            await bathCat.RequestStream.CompleteAsync();
+            Console.WriteLine("sent 10 id");
+            Console.WriteLine("receive result: ");
+
+            foreach (var item in bathCat.ResponseAsync.Result.Number)
+            {
+                Console.WriteLine($"This is {item} Result");
+            }
+            Console.WriteLine("**********************************");
+        }
+        Console.WriteLine("**************************Server side flow *****************************");
+        {
+            IntArrayModel intArrayModel = new IntArrayModel();
+            for (int i = 0; i < 15; i++)
+            {
+                intArrayModel.Number.Add(i);//cannot assign value directly to Number
+            }
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            cts.CancelAfter(TimeSpan.FromSeconds(5.5)); //cancel after 5.5s
+            var bathCat = client.SelfIncreaseServer(intArrayModel, cancellationToken: cts.Token);
+
+            //var bathCat = client.SelfIncreaseServer(intArrayModel);//no cancel token
+            var bathCatRespTask = Task.Run(async () =>
+            {
+                await foreach (var resp in bathCat.ResponseStream.ReadAllAsync())
+                {
+                    Console.WriteLine(resp.Message);
+                    Console.WriteLine($"This is  Response {Thread.CurrentThread.ManagedThreadId}");
+                    Console.WriteLine("**********************************");
+                }
+            });
+            Console.WriteLine("客户端已发送完10个id");
+            //start receiving  response
+            await bathCatRespTask;
+        }
+
+
+
+
+
+
+
     }
+
+
+
+
+
+
 }
 
 
--- P05.gRPC.DemoServer/Services/CustomMathService.cs
+++ P05.gRPC.DemoServer/Services/CustomMathService.cs
@@ -1,17 +1,96 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace P05.gRPC.DemoServer.Services
 {
-    public class CustomMathService:CustomMath.CustomMathBase
+    public class CustomMathService : CustomMath.CustomMathBase
     {
+        private readonly ILogger<CustomMathService> _logger;
+        public CustomMathService(ILogger<CustomMathService> logger)
+        {
+            _logger = logger;
+        }
+
         public override Task<HelloReplyMath> SayHello(HelloRequestMath request, ServerCallContext context)
         {
+            Console.WriteLine("-----------CustomMathService.SayHello() service   " );
             return Task.FromResult<HelloReplyMath>(new HelloReplyMath()
             {
                 Message = $"This is {request.Name} id: {request.Id}"
             });
         }
+
+
+        public override Task<CountResult> Count(Empty request, ServerCallContext context)
+        {
+            Console.WriteLine("-----------CustomMathService.Count() service   " );
+            return Task.FromResult(new CountResult()
+            {
+                Count = DateTime.Now.Year
+            });
+        }
+
+        public override Task<ResponseResult> Plus(RequestPara request, ServerCallContext context)
+        {
+            Console.WriteLine("-----------CustomMathService.Plus() service   "   );
+            int iResult = request.ILeft + request.IRight;
+            ResponseResult responseResult = new ResponseResult()
+            {
+                Result = iResult,
+                Message = "Success"
+            };
+
+            return Task.FromResult(responseResult);
+        }
+
+
+        public override async Task SelfIncreaseServer(IntArrayModel request, IServerStreamWriter<BathTheCatResp> responseStream, ServerCallContext context)
+        {
+            Console.WriteLine("-----------CustomMathService.SelfIncreaseServer() service   " );
+
+            foreach (var item in request.Number)
+            {
+                int number = item;
+                Console.WriteLine($"This is {number} invoke");
+                await responseStream.WriteAsync(new BathTheCatResp() { Message = $"number++ ={++number}！" });
+                await Task.Delay(500);
+            }
+        }
+
+        public override async Task<IntArrayModel> SelfIncreaseClient(IAsyncStreamReader<BathTheCatReq> requestStream, ServerCallContext context)
+        {
+            Console.WriteLine("-----------CustomMathService.SelfIncreaseServer() service   " );
+
+            IntArrayModel intArrayModel = new IntArrayModel();
+            while (await requestStream.MoveNext())
+            {
+                intArrayModel.Number.Add(requestStream.Current.Id + 1);
+                Console.WriteLine($"SelfIncreaseClient Number {requestStream.Current.Id} received and process..");
+                Thread.Sleep(100);
+            }
+            return intArrayModel;
+        }
+
+        public override async Task SelfIncreaseDouble(IAsyncStreamReader<BathTheCatReq> requestStream, IServerStreamWriter<BathTheCatResp> responseStream, ServerCallContext context)
+        {
+            Console.WriteLine("-----------CustomMathService.SelfIncreaseDouble() service   " );
+
+            while (await requestStream.MoveNext())
+            {
+                Console.WriteLine($"SelfIncreaseDouble Number {requestStream.Current.Id} received and process..");
+                await responseStream.WriteAsync(new BathTheCatResp() { Message = $"number++ ={requestStream.Current.Id + 1}！" });
+                await Task.Delay(500);
+            }
+
+        }
+
+
+
 
     }
 

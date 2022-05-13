f15e2e02d2b7f70bf18e5c9be7d51380a0d1f0cf
adrian_sun <ziqingyi@qq.com>
2022-05-13 10:46:29 +1000

    blazor wasm server with start up

--- P06.BlazorWebAssemblyApp/Server/Program.cs
+++ P06.BlazorWebAssemblyApp/Server/Program.cs
@@ -1,36 +1,23 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using P06.BlazorWebAssemblyApp.Server;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
+#region add startup
+
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
+
+#endregion
+
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseWebAssemblyDebugging();
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
 
 app.UseRouting();
 
-
-app.MapRazorPages();
-app.MapControllers();
-app.MapFallbackToFile("index.html");
 
 app.Run();
---
+++ P06.BlazorWebAssemblyApp/Server/Startup.cs
@@ -0,0 +1,70 @@
+﻿
+
+
+namespace P06.BlazorWebAssemblyApp.Server
+{
+    public class Startup
+    {
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+            services.AddControllersWithViews();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
+            services.AddServerSideBlazor();
+            //services.AddSingleton<WeatherForecastService>();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseBlazorFrameworkFiles();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                //core 3.1
+                //endpoints.MapBlazorHub();
+                //endpoints.MapFallbackToPage("/_Host");
+
+
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+                endpoints.MapFallbackToFile("index.html");
+
+            });
+
+
+        }
+
+    }
+
+
+
+}

f5886c70b1affa434959ed0909eecc75434b95c3
adrian_sun <ziqingyi@qq.com>
2022-05-16 15:00:43 +1000

    route parameters from page to page

---
+++ P06.BlazorServerApp/Data/Member.cs
@@ -0,0 +1,10 @@
+﻿namespace P06.BlazorServerApp.Data
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int TeamId { get; set; }
+    }
+}
---
+++ P06.BlazorServerApp/Data/Team.cs
@@ -0,0 +1,9 @@
+﻿namespace P06.BlazorServerApp.Data
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Region { get; set; }
+    }
+}

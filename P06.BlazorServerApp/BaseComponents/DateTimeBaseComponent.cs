5ec1615dfb115de5781b87c4130f689949f33393
adrian_sun <ziqingyi@qq.com>
2022-05-16 13:41:50 +1000

    inherites DateTimeBaseComponent , OrderEntryComponent, adding TitleComponent with param

---
+++ P06.BlazorServerApp/BaseComponents/DateTimeBaseComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace P06.BlazorServerApp.BaseComponents
+{
+    public class DateTimeBaseComponent:ComponentBase
+    {
+        public DateTime dateTime { get; set; } = DateTime.Now.ToLocalTime();
+
+    }
+
+}

64d9d8bd49fac2e05abbfdc61328d2fca86b26dd
adrian_sun <ziqingyi@qq.com>
2022-05-23 11:33:24 +1000

    generic typed templete component

---
+++ P06.BlazorWebAssemblyApp/Shared/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P06.BlazorWebAssemblyApp.Shared
+{
+    public class Customer
+    {
+        public string ContactName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string CompanyName { get; set; }
+
+    }
+}

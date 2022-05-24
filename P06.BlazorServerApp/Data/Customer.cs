8214ac9638aa14e8b6c397d1418a12e61931fffb
adrian_sun <ziqingyi@qq.com>
2022-05-24 17:04:16 +1000

    long time loading

---
+++ P06.BlazorServerApp/Data/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace P06.BlazorServerApp.Data
+{
+    public class Customer
+    {
+        public string ContactName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string CompanyName { get; set; }
+
+    }
+}
---
+++ P06.BlazorServerApp/Data/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace P06.BlazorServerApp.Data
+{
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Department { get; set; }
+
+    }
+}

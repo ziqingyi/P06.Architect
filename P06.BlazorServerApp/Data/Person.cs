28893ae248a686fa3d07796b952ebea1de3894ec
adrian_sun <ziqingyi@qq.com>
2022-05-25 11:08:12 +1000

    editForm

---
+++ P06.BlazorServerApp/Data/Person.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P06.BlazorServerApp.Data
+{
+    public class Person
+    {
+        [Required] 
+        [Range(1, 10000)] 
+        public int EmployeeId { get; set; }
+
+        [Required] 
+        [StringLength(100)] 
+        public string FirstName { get; set; } 
+
+        [Required] 
+        [StringLength(100)] 
+        public string LastName { get; set; }
+
+        [Required] 
+        [EmailAddress] 
+        public string Email { get; set; } 
+
+
+
+
+    }
+}

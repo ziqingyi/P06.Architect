024c3f653bdba06a4ebdc3c7903e2fb0f60ec9b1
adrian_sun <ziqingyi@qq.com>
2022-06-06 15:35:34 +1000

    WebMSTestProject

---
+++ P03.DotNetCoreMVC.WebMSTestProject/Mathematics_Test.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using P03.DotNetCoreMVC.Utility.WebHelper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03.DotNetCoreMVC.WebMSTestProject
+{
+    [TestClass]
+    public class Mathematics_Test
+    {
+        [TestMethod]
+        public void Test_Divide()
+        {
+            var expectValue = 5;
+            var dividend = 10;
+            var divisor = 2;
+
+            //
+            int actuallValue = Mathematics.Divide(dividend, divisor);
+
+            Assert.AreEqual(expectValue, actuallValue);
+        }
+
+
+
+        [TestMethod]
+        public void Divide_PositiveNumbers_ReturnsPositiveQuotient()
+        {
+            var expectValue = 5;
+            var dividend = 10;
+            var divisor = 2;
+
+            //
+            int actuallValue = Mathematics.Divide(dividend, divisor);
+
+            Assert.AreEqual(expectValue, actuallValue);
+
+        }
+
+    }
+}
